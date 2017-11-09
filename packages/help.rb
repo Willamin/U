ec318772ae00efa9d80af3f4d500b6fb8548e887
Caleb Albritton <me@wa9ace.net>
2017-11-09 11:18:30 -0500

    added args to help

--- packages/help.rb
+++ packages/help.rb
@@ -2,8 +2,17 @@ class Help < U::Package
   VERSION = U::VERSION # use "x.x.x" for your own packages
 
   script 'help' do
-    scripts = U::Package.scripts.map { |scriptname, _| scriptname }
+    scripts = U::Package.scripts.map do |name, block|
+      params = get_script_params(block)
+
+      if !params.empty?
+        "#{name}: #{params.join(' ')}"
+      else
+        name
+      end
+    end
+
     scripts.insert(0, "available scripts:")
-    uprint scripts.join("\n\t  ")
+    uprint scripts.join("\n\t ")
   end
 end
